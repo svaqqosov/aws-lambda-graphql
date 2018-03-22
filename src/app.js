a7e4e6ac2d21d667fff206aecd26a3d2ada15038
Sardor <sardor@next-aeon.com>
2018-03-22 19:36:47 +0900

    first commit

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const body_parser = require('body-parser');
+
+const expressGraphQL = require('express-graphql');
+
+// let's import the schema file we just created
+const GraphQLSchema = require('./graphql/schema');
+
+
+const app = express();
+
+app.use(body_parser.json({ limit: '50mb' }));
+
+app.use(
+  '/',
+  expressGraphQL(() => ({
+    graphiql: true,
+    schema: GraphQLSchema
+  }))
+);
+
+module.exports = app;
